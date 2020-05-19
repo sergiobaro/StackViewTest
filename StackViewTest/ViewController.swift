ee7c3324396f6082c7d3f78e8fb22ed23b31b2ac
Sergio Baró <serbarsim@gmail.com>
2020-05-19 09:04:49 +0200

    Initial Commit

---
+++ StackViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewTest
+//
+//  Created by Sergio Baro on 19/05/2020.
+//  Copyright © 2020 Sergio Baro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
